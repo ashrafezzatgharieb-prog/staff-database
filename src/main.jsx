4c1d9284d70096c500dfff1688af520d78a9a030
ashrafezzatgharieb-prog <ashraf.ezzat.gharieb@gmail.com>
2026-06-07 16:07:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../staff-database.jsx'
+import App from './staff-database.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+)
